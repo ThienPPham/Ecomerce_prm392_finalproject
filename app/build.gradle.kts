b2c69944c356fb85edfbba4b4b35727bb9083dbe
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-06-26 18:32:35 +0700

    RecyclerView Adapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,10 +39,16 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
     //slider image
     implementation("com.github.denzcoskun:ImageSlideshow:0.1.2") // Corrected syntax
+
+    //Glide
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
 }
