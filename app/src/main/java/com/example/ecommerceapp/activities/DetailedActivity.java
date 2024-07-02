4d67c5dcd09da757d86a28815616e34316444b12
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-07-02 16:17:09 +0700

    Pass Data One Activity To Another

--- app/src/main/java/com/example/ecommerceapp/activities/DetailedActivity.java
+++ app/src/main/java/com/example/ecommerceapp/activities/DetailedActivity.java
@@ -1,6 +1,9 @@
 package com.example.ecommerceapp.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
 import com.example.ecommerceapp.R;
+import com.example.ecommerceapp.models.NewProductsModel;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.ktx.Firebase;
+
+import org.w3c.dom.Text;
 
 public class DetailedActivity extends AppCompatActivity {
+
+    ImageView detailedImg;
+    TextView rating, name, description, price;
+    Button addToCart, buyNow;
+    ImageView addItems, removeItems;
+
+    //New Products
+    NewProductsModel newProductsModel = null;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +40,35 @@ public class DetailedActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        firestore = FirebaseFirestore.getInstance();
+
+        final Object obj = getIntent().getSerializableExtra("detailed");
+
+        if (obj instanceof NewProductsModel){
+            newProductsModel = (NewProductsModel) obj;
+        }
+
+        detailedImg = findViewById(R.id.detailed_img);
+        name = findViewById(R.id.detailed_name);
+        rating = findViewById(R.id.rating);
+        description = findViewById(R.id.detailed_desc);
+        price = findViewById(R.id.detailed_price);
+
+        addToCart = findViewById(R.id.add_to_cart);
+        buyNow = findViewById(R.id.buy_now);
+
+        addItems = findViewById(R.id.add_item);
+        removeItems = findViewById(R.id.remove_item);
+
+        //New Products
+        if (newProductsModel != null){
+            Glide.with(getApplicationContext()).load(newProductsModel.getImg_url()).into(detailedImg);
+            name.setText(newProductsModel.getName());
+            rating.setText(newProductsModel.getRating());
+            description.setText(newProductsModel.getDescription());
+            price.setText(String.valueOf(newProductsModel.getPrice()));
+            name.setText(newProductsModel.getName());
+        }
     }
 }
--- app/src/main/java/com/example/ecommerceapp/adapters/NewProductsAdapter.java
+++ app/src/main/java/com/example/ecommerceapp/adapters/NewProductsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.ecommerceapp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.ecommerceapp.R;
+import com.example.ecommerceapp.activities.DetailedActivity;
 import com.example.ecommerceapp.models.NewProductsModel;
 
 import java.util.List;
@@ -38,6 +40,15 @@ public class NewProductsAdapter extends RecyclerView.Adapter<NewProductsAdapter.
         Glide.with(context).load(list.get(position).getImg_url()).into(holder.newImg);
         holder.newName.setText(list.get(position).getName());
         holder.newPrice.setText(String.valueOf(list.get(position).getPrice()));
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailedActivity.class);
+                intent.putExtra("detailed", list.get(position));
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
