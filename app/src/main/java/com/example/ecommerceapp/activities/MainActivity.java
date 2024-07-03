61a97d78b2f00ae33ab3744451be3899f8683178
ThienPPham <thienversiopp129.it.vn@gmail.com>
2024-07-03 15:02:44 +0700

    Toolbar

--- app/src/main/java/com/example/ecommerceapp/activities/MainActivity.java
+++ app/src/main/java/com/example/ecommerceapp/activities/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.ecommerceapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,26 +17,52 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.ecommerceapp.R;
 import com.example.ecommerceapp.fragments.HomeFragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     Fragment homeFragment;
+    Toolbar toolbar;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth =FirebaseAuth.getInstance();
+        toolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+
         homeFragment = new HomeFragment();
         loadFragment(homeFragment);
     }
 
     private void loadFragment(Fragment homeFragment) {
-
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.home_container, homeFragment);
         transaction.commit();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_logout){
+            auth.signOut();
+            startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            finish();
+        } else if (id == R.id.menu_my_cart) {
+            startActivity(new Intent(MainActivity.this, CartActivity.class));
+        }
+        return true;
+    }
 }
